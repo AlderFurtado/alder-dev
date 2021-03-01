036187259aedecdb69658811e75e16ed642ce3ef
AlderFurtado <aldermarcel26@outlook.com>
2021-03-01 01:02:35 -0300

    fix design add meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,6 +16,26 @@ export default function Home(): JSX.Element {
       <Head>
         <title>Alder Furtado</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Site profissional de desenvolvedor Full Stack - Alder Furtado"
+        />
+        <meta
+          name="keywords"
+          content="Alder, JavaScript, Dev, Desenvolvedor, Programação"
+        ></meta>
+        <meta name="author" content="Alder Furtado"></meta>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+        <meta property="og:title" content="Alder Dev" />
+        <meta
+          property="og:description"
+          content="Site profissional de desenvolvedor Full Stack - Alder Furtado"
+        />
+        <meta property="og:image" content="./me.png" />
+        <meta property="og:url" content="https://alder-dev.vercel.app" />
       </Head>
       <Hero />
       <About />
