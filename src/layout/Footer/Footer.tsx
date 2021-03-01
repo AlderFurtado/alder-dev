020855e9d4645de45d3960f7134e43b34f52f422
AlderFurtado <aldermarcel26@outlook.com>
2021-02-28 23:39:28 -0300

    add footer

---
+++ src/layout/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+const Footer = (): JSX.Element => {
+  return (
+    <footer className={styles.wrapper}>
+      <p>©Copyright - Alder Furtado</p>
+    </footer>
+  );
+};
+
+export default Footer;
