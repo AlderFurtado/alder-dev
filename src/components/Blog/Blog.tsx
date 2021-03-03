a7aac91a0c1d38298658ca902de2b7cb46130330
AlderFurtado <aldermarcel26@outlook.com>
2021-03-02 21:48:41 -0300

    add blog component

---
+++ src/components/Blog/Blog.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./Blog.module.css";
+
+const Blog: React.FC = () => {
+  return (
+    <section className={styles.wrapper}>
+      <h2>
+        Estamos trabalhando nisso.
+        <br /> Sabemos que software não são feitos do dia para noite
+        <br /> mas logo sairá essa novidade 😅
+      </h2>
+    </section>
+  );
+};
+
+export default Blog;
