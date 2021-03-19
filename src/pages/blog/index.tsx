36f32d79da8a4313661883f9de8c7e2fa813d328
AlderFurtado <aldermarcel26@outlook.com>
2021-03-18 23:39:27 -0300

    head seo add in blog

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -5,6 +5,7 @@ import { GetStaticProps } from "next";
 import { getPosts } from "../../services/prismic";
 import ResponsePrismic from "../../types/ResponsePrismic";
 import Footer from "../../layout/Footer/Footer";
+import HeadSeo from "../../seo/HeadSeo";
 
 interface BlogProps {
   postsResponse: ResponsePrismic;
@@ -13,6 +14,16 @@ interface BlogProps {
 const blog = ({ postsResponse }: BlogProps): JSX.Element => {
   return (
     <>
+      <HeadSeo
+        title={"Alder Furtado"}
+        description={
+          "Site profissional de desenvolvedor Full Stack - Alder Furtado"
+        }
+        keywords={"Alder, JavaScript, Dev, Desenvolvedor, Programação"}
+        author={"Alder Furtado"}
+        image={"https://i.ibb.co/YBLKFSf/IMG-20200807-191408-106-1.png"}
+        url={"https://alder-dev.vercel.app"}
+      />
       <Menu />
       <Blog postsResponse={postsResponse} />
       <Footer />
